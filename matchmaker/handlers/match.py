8be1e1294d3ba7f931c00526d35f6c96f218855f
MomusWinner <areg06.06@mail.ru>
2024-10-29 21:28:00 +0300

    update matchmaker/handler remove unused import

--- matchmaker/handlers/match.py
+++ matchmaker/handlers/match.py
@@ -1,11 +1,5 @@
 
-import aio_pika
-import msgpack
-from aio_pika import ExchangeType
-from sqlalchemy import select, func
-
 from config.settings import settings
-# from matchmaker.model.gift import Gift
 from shared.schema.messages.match import GetMatchMessage
 from matchmaker.storage.db import async_session
 from matchmaker.storage.rabbit import channel_pool
