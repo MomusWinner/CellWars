96629700c76f5519e93dc3fd639602691a900d91
spl3g <spleefer6@yandex.ru>
2024-10-29 20:13:43 +0300

    implement the main menu

--- src/handlers/command/start.py
+++ src/handlers/command/start.py
@@ -1,38 +1,21 @@
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, \
-    InlineKeyboardMarkup
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 from .router import router
-from ..states.auth import AuthGroup
+from ..states.menu import MenuGroup
+from ..buttons import MATCHMAKING_INLINE, STATS_INLINE
 
 
 @router.message(Command('start'))
 async def start_cmd(message: Message, state: FSMContext) -> None:
-    await state.set_state(AuthGroup.no_authorized)
-
-    await state.set_data({
-        'button1': 1,
-        'button2': 1,
-    })
+    await state.set_state(MenuGroup.start)
 
     # callback buttons
-    inline_btn_1 = InlineKeyboardButton(text='Первая кнопка!', callback_data='button1')
-    inline_btn_2 = InlineKeyboardButton(text='Вторая кнопка!', callback_data='button2')
-    markup = InlineKeyboardMarkup(
-        inline_keyboard=[[inline_btn_1, inline_btn_2]]
+    matchmaking = InlineKeyboardButton(
+        text=MATCHMAKING_INLINE["text"], callback_data=MATCHMAKING_INLINE["callback_data"]
     )
+    stats = InlineKeyboardButton(text=STATS_INLINE["text"], callback_data=STATS_INLINE["callback_data"])
+    markup = InlineKeyboardMarkup(inline_keyboard=[[matchmaking], [stats]])
 
-    # text buttons
-    # button1 = KeyboardButton(text='1️⃣')
-    # button2 = KeyboardButton(text='2️⃣')
-    # button3 = KeyboardButton(text='3️⃣')
-    # markup = ReplyKeyboardMarkup(keyboard=[[button1, button2, button3]])
-
-    await message.answer('Hello!', reply_markup=markup)
-
-
-@router.message()
-async def start_cmd(message: Message, state: FSMContext) -> None:
-    await state.set_state(AuthGroup.authorized)
-    await message.answer('Hello asdasd!')
+    await message.answer('Привет!', reply_markup=markup)
