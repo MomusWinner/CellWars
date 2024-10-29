254e6455425d9ef32346bfb7fd564f3b07eb20d6
MomusWinner <areg06.06@mail.ru>
2024-10-29 21:25:39 +0300

    update handle_event_create_match

--- game/handlers/match.py
+++ game/handlers/match.py
@@ -1,7 +1,12 @@
+from redis import Redis
 from shared.schema.messages.match import CreateMatchMessage
+from game.storage.redis import get_redis
+from uuid import uuid4, UUID
 
-
-def handle_event_create_match(message: CreateMatchMessage):
+async def handle_event_create_match(message: CreateMatchMessage):
     user_ids: list[int] = message["user_ids"]
     print("create room for user_ids ", user_ids)
+    room_id: UUID = uuid4()
+    redis: Redis = get_redis()
+    await redis.json().set(str(room_id), "$", {"test": "test_room"})
     ## create room
