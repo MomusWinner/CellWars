5c09673711c6856a97e647c3391a26a288f1edce
MomusWinner <areg06.06@mail.ru>
2024-11-18 22:37:55 +0300

    refactor warriors object

--- src/my_app/shared/game/game_logic/game_objects.py
+++ src/my_app/shared/game/game_logic/game_objects.py
@@ -54,48 +54,69 @@ class Warriors(GameObject):
         super().__init__(cell, game_world, player)
         self.count = count
 
+    def _change_position(self, position: Position) -> None:
+        self.game_world.clean_cell(self.cell.position)
+        self.game_world.set_object_to_cell(position, self)
+
+    def _fight_with_warriors(self, other_warriors: Warriors) -> None:
+        other_w_position = other_warriors.cell.position
+
+        if other_warriors.player == self.player:
+            other_warriors.count += self.count
+            return
+
+        if other_warriors.count > self.count:
+            other_warriors.count -= self.count
+            self.game_world.destroy(self)
+        elif self.count > other_warriors.count:
+            self.count -= other_warriors.count
+            self.game_world.destroy(other_warriors)
+            self._change_position(other_w_position)
+        else:
+            self.game_world.destroy(self)
+            self.game_world.destroy(other_warriors)
+
+    def _attack_vulnerable(self, vulnerable: Vulnerable):
+        v_pos = vulnerable.cell.position
+        damage = 0
+        if vulnerable.hp >= self.count:
+            damage = self.count
+        elif vulnerable.hp < self.count:
+            damage = vulnerable.hp
+
+        target_is_destroyed = vulnerable.set_damage(damage)
+        self.count -= damage
+
+        if self.count <= 0:
+            self.game_world.destroy(self)
+        elif target_is_destroyed:
+            self._change_position(v_pos)
+
     def _move(self, target: Position):
         target_object = self.game_world.get_object_by_position(target)
         if target_object is None:
-            self.game_world.clean_cell(self.cell.position)
-            self.game_world.set_object_to_cell(target, self)
-
+            self._change_position(target)
         elif isinstance(target_object, Warriors):
-            if target_object.player == self.player:
-                target_object.count += self.count
-            else:
-                if target_object.count > self. count:
-                    self.game_world.destroy(self)
-                elif self.count > target_object.count:
-                    self.game_world.destroy(target_object)
-                    self.game_world.clean_cell(self.cell.position)
-                    self.game_world.set_object_to_cell(target, self)
-                else:
-                    self.game_world.destroy(self)
-                    self.game_world.destroy(target_object)
+            self._fight_with_warriors(target_object)
         elif target_object.player == self.player:
             raise IncorrectMovementPositionException
         elif isinstance(target_object, Vulnerable):
-            self.game_world.clean_cell(self.cell.position)
-            target_object: Vulnerable
-            damage = 0
-            if target_object.hp >= self.count:
-                damage = self.count
-            elif target_object.hp < self.count:
-                damage = target_object.hp
-
-            target_is_destroyed = target_object.set_damage(damage)
-            self.count -= damage
-
-            if self.count <= 0:
-                self.game_world.destroy(self)
-            elif target_is_destroyed:
-                self.game_world.set_object_to_cell(target, self)
+            self._attack_vulnerable(target_object)
         else:
             raise IncorrectMovementPositionException
 
-    def move(self, target: Position) -> bool:
+    def move(self, target: Position):
         if (self.game_world.distance(self.cell.position, target) > 2):
             raise IncorrectMovementPositionException
-        self._move(target)
-        return True
+        
+        target_object = self.game_world.get_object_by_position(target)
+        if target_object is None:
+            self._change_position(target)
+        elif isinstance(target_object, Warriors):
+            self._fight_with_warriors(target_object)
+        elif target_object.player == self.player:
+            raise IncorrectMovementPositionException
+        elif isinstance(target_object, Vulnerable):
+            self._attack_vulnerable(target_object)
+        else:
+            raise IncorrectMovementPositionException
