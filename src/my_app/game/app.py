35cf630231f711b429eae530071c1a36296a1c20
MomusWinner <areg06.06@mail.ru>
2024-11-20 20:50:40 +0300

    setup logger for game

--- src/my_app/game/app.py
+++ src/my_app/game/app.py
@@ -1,13 +1,17 @@
+import logging.config
 import aio_pika
 import msgpack
 
 from my_app.game.storage.redis import setup_redis
 from my_app.game.handlers.match_queue_handler import handle_matches
 from my_app.game.handlers.game_queue_handler import handle_games
+from my_app.game.logger import logger, LOGGING_CONFIG
 
 import asyncio
 
 async def main() -> None:
+    logging.config.dictConfig(LOGGING_CONFIG)
+    logger.info('Starting consumer...')
     setup_redis()
     await asyncio.gather(
         asyncio.create_task(handle_matches()),
