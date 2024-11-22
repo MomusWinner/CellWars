8ea1de20616945093b466c1743721a4eb7759262
spl3g <spleefer6@yandex.ru>
2024-11-22 17:49:01 +0300

    add start menu message

---
+++ src/my_app/bot/messages/menu.py
@@ -0,0 +1,13 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from my_app.bot.handlers.buttons import MATCHMAKING_INLINE, STATS_INLINE
+
+
+async def start_menu() -> tuple[str, InlineKeyboardMarkup]:
+    matchmaking = InlineKeyboardButton(
+        text=MATCHMAKING_INLINE["text"], callback_data=MATCHMAKING_INLINE["callback_data"]
+    )
+    stats = InlineKeyboardButton(text=STATS_INLINE["text"], callback_data=STATS_INLINE["callback_data"])
+    markup = InlineKeyboardMarkup(inline_keyboard=[[matchmaking], [stats]])
+
+    return "Привет!", markup
