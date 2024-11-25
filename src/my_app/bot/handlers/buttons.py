e498ac722157131ddf121eda1a29d8d9771007e8
spl3g <spleefer6@yandex.ru>
2024-11-25 14:41:39 +0300

    add field callback data type

--- src/my_app/bot/handlers/buttons.py
+++ src/my_app/bot/handlers/buttons.py
@@ -1,3 +1,5 @@
+from aiogram.filters.callback_data import CallbackData
+
 MATCHMAKING_INLINE = {
     "text": "Начать поиск матча",
     "callback_data": "matchmake",
@@ -12,3 +14,9 @@ CANCEL_MATCHMAKING_INLINE = {
     "text": "Отмена",
     "callback_data": "cancel_matchmaking",
 }
+
+
+class FieldCallback(CallbackData, prefix="field"):
+    cell_x: int
+    cell_y: int
+    type: str
