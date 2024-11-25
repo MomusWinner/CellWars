8033dea9a9ffbc5e57974215d98931f7b516d44d
spl3g <spleefer6@yandex.ru>
2024-11-25 21:33:44 +0300

    change render field to account for the new api

--- src/my_app/bot/composables/field.py
+++ src/my_app/bot/composables/field.py
@@ -1,13 +1,13 @@
 from aiogram.types import InlineKeyboardButton
 from my_app.bot.handlers.buttons import FieldCallback
-from my_app.bot.utils.field import rotate_field, get_icon, get_type
+from my_app.bot.utils.field import get_icon, get_type, rotate_field
 from my_app.shared.game.game_logic.core import GameWorld
 
 
-def render_field(game_world: GameWorld, user_id: int) -> list[list[InlineKeyboardButton]]:
-    field = rotate_field(game_world.cells, user_id)
-    field_markup = []
+def render_field(game_world: GameWorld) -> list[list[InlineKeyboardButton]]:
+    field = rotate_field(game_world.cells, game_world.player_by_tag[0])
 
+    field_markup: list[list[InlineKeyboardButton]] = []
     for row_idx, row in enumerate(field):
         markup_row = []
         for cell in range(len(row)):
@@ -19,7 +19,9 @@ def render_field(game_world: GameWorld, user_id: int) -> list[list[InlineKeyboar
             markup_row.append(
                 InlineKeyboardButton(
                     text=get_icon(game_object, tag),
-                    callback_data=FieldCallback(cell_x=cell, cell_y=row_idx, type=get_type(game_object)).pack(),
+                    callback_data=FieldCallback(
+                        cell_x=cell, cell_y=row_idx, type=get_type(game_object)
+                    ).pack(),
                 )
             )
         field_markup.append(markup_row)
