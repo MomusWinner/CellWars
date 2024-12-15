57cbfb20d068fcd35753a9a62a572ebbd8576b8a
spl3g <spleefer6@yandex.ru>
2024-12-15 18:30:24 +0300

    add game message class

---
+++ src/my_app/bot/types/game.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass, field
+
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from my_app.bot.composables import actions
+
+
+class GameMessage:
+    info: str
+    actions: list[list[InlineKeyboardButton]]
+    field: list[list[InlineKeyboardButton]]
+
+    def __init__(
+        self,
+        info: str | None,
+        actions: list[list[InlineKeyboardButton]] | None,
+        field: list[list[InlineKeyboardButton]] | None,
+    ):
+        self.info = info if info else ""
+        self.actions = actions if actions else []
+        self.field = field if field else []
+
+    @classmethod
+    def from_markup(
+        cls: type["GameMessage"], info: str, markup: InlineKeyboardMarkup
+    ) -> "GameMessage":
+        return cls.from_buttons(info=info, buttons=markup.inline_keyboard)
+
+    @classmethod
+    def from_buttons(
+        cls: type["GameMessage"], info: str, buttons: list[list[InlineKeyboardButton]]
+    ) -> "GameMessage":
+        actions: list[list[InlineKeyboardButton]] = []
+        field: list[list[InlineKeyboardButton]] = []
+        for i, row in enumerate(buttons):
+            if row[0].callback_data is not None and not row[0].callback_data.startswith(
+                "action"
+            ):
+                actions = buttons[: i + 1]
+                field = buttons[i:]
+                break
+
+        return cls(info, actions, field)
+
+    @classmethod
+    def from_field(
+        cls: type["GameMessage"], field: list[list[InlineKeyboardButton]]
+    ) -> "GameMessage":
+        return cls(info=None, actions=None, field=field)
+
+    def export_markup(self) -> InlineKeyboardMarkup:
+        return InlineKeyboardMarkup(inline_keyboard=self.actions + self.field)
