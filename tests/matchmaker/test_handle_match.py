f57c0c5dbce7ca31eb8d2df9854c4d4c959892de
MomusWinner <areg06.06@mail.ru>
2024-12-18 21:13:56 +0300

    move game test to matchmaker

--- tests/matchmaker/test_handle_match.py
+++ tests/matchmaker/test_handle_match.py
@@ -1,8 +1,10 @@
+import asyncio
 from typing import Any
 import pytest
 from my_app.shared.schema.messages.match import MatchMessage
 from my_app.matchmaker.app import main
-# from src.my_app.matchmaker.storage.rabbit import
+from tests.mocking.rabbitmq import MockExchange
+
 
 @pytest.mark.parametrize(
     ("predefined_queue"),
@@ -17,5 +19,6 @@ from my_app.matchmaker.app import main
 )
 @pytest.mark.asyncio()
 @pytest.mark.usefixtures("_load_queue")
-async def test_first(predefined_queue: list[Any]) -> None:
+async def test_first(predefined_queue: list[Any], mock_exchange: MockExchange) -> None:
     await main()
+    assert len(mock_exchange.method_calls) != 0
