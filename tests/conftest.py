c8a0f6842c4ebdd773d1ad6c03fbc232927a6136
MomusWinner <areg06.06@mail.ru>
2024-12-18 21:16:27 +0300

    update imports and fix incorrect predefined_queue processing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,28 @@
 
 from collections import deque
 from typing import Any
+from uuid import uuid4
+
 import aio_pika
 import msgpack
 import pytest
 import pytest_asyncio
-from uuid import uuid4
-import asyncio
+
 from my_app.matchmaker.storage import rabbit
 
-from .mocking.rabbitmq import MockChannel, MockChannelPool, MockExchange, MockExchangeMessage, MockQueue
+from .mocking.rabbitmq import (
+    MockChannel,
+    MockChannelPool,
+    MockExchange,
+    MockExchangeMessage,
+    MockQueue,
+)
+
 
 @pytest.fixture()
 def mock_exchange() -> MockExchange:
     return MockExchange()
+
 
 @pytest_asyncio.fixture()
 async def _load_queue(
@@ -21,10 +30,11 @@ async def _load_queue(
     predefined_queue: list[Any],
     mock_exchange: MockExchange
 ) -> None:
-    print(predefined_queue)
     queue = MockQueue(deque())
     if predefined_queue is not None:
-        await queue.put(msgpack.packb(predefined_queue), str(uuid4()))
+        for messages in predefined_queue:
+            for message in messages:
+                await queue.put(msgpack.packb(message), str(uuid4()))
 
     channel = MockChannel(queue=queue, exchange=mock_exchange)
     pool = MockChannelPool(channel=channel)
