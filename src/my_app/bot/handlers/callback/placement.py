b754f08b264b06627f72607e322ab040ba49dde7
spl3g <spleefer6@yandex.ru>
2024-12-20 21:51:34 +0300

    add bank placement handler

--- src/my_app/bot/handlers/callback/placement.py
+++ src/my_app/bot/handlers/callback/placement.py
@@ -1,3 +1,6 @@
+import aio_pika
+import msgpack
+from aiogram import F
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, Message
 from my_app.bot.composables.actions import add_cancel_button, add_field_actions
@@ -14,11 +17,18 @@ from my_app.bot.handlers.buttons import (
 from my_app.bot.storage.rabbit import channel_pool
 from my_app.bot.types.callbacks import PlacementCallback
 from my_app.bot.types.game import GameTGMessage
+from my_app.bot.utils.field import rotate_coordinates
+from my_app.shared.game.game_logic.command import (
+    BuildBank,
+    BuyWarriorsCommand,
+    PositionCommand,
+    create_build_bank_command,
+)
 from my_app.shared.game.game_logic.serialize_deserialize_game_world import (
     json_to_game_world,
 )
 from my_app.shared.rabbit.game import GAME_EXCHANGE, GAME_QUEUE
-from my_app.shared.schema.messages.game import GameMessage
+from my_app.shared.schema.messages.game import GameMessage, create_game_message
 
 from .router import router
 
@@ -68,9 +78,7 @@ async def bank_placement_intent_handler(
 
 
 @router.callback_query(CANCEL_FIELD_INLINE())
-async def cancel_placement_handler(
-    callback_query: CallbackQuery, state: FSMContext
-) -> None:
+async def cancel_placement_handler(callback_query: CallbackQuery, state: FSMContext) -> None:
     if not isinstance(callback_query.message, Message):
         await callback_query.answer("what")
         return
@@ -90,3 +98,52 @@ async def cancel_placement_handler(
     with_actions = add_field_actions(GameTGMessage.from_field(field_buttons))
 
     await message.edit_reply_markup(reply_markup=with_actions.export_markup())
+
+
+@router.callback_query(PlacementCallback.filter(F.type == "bank"))
+async def place_bank_handler(
+    callback_query: CallbackQuery, callback_data: PlacementCallback, state: FSMContext
+) -> None:
+    if not isinstance(callback_query.message, Message):
+        await callback_query.answer("what")
+        return
+
+    data = await state.get_data()
+    room_id: str = data["room_id"]
+    user_tag: int = data["user_tag"]
+
+    message = callback_query.message
+    reply_markup = message.reply_markup
+    if reply_markup is None or message.text is None:
+        return
+    game_tg_message = GameTGMessage.from_markup(message.text, reply_markup)
+    len_y = len(game_tg_message.field)
+    len_x = len(game_tg_message.field[0])
+    point = (callback_data.cell_y, callback_data.cell_x)
+    point_rotated = rotate_coordinates(point, len_x, len_y, user_tag)
+    position: PositionCommand = {"x": point_rotated[0], "y": point_rotated[1]}
+    print(user_tag)
+    print(position)
+
+    channel: aio_pika.Channel
+    async with channel_pool.acquire() as channel:
+        queue = await channel.declare_queue(GAME_QUEUE, durable=True)
+        exchange = await channel.declare_exchange(GAME_EXCHANGE, aio_pika.ExchangeType.DIRECT, durable=True)
+
+        await queue.bind(exchange)
+        game_message_dict = create_game_message(
+            room_id=room_id,
+            command=create_build_bank_command(user_id=callback_query.from_user.id, position=position),
+        )
+
+        body_exchange: bytes | None = msgpack.packb(game_message_dict)
+        if body_exchange is None:
+            return
+
+        await exchange.publish(
+            aio_pika.Message(
+                body_exchange,
+            ),
+            routing_key=GAME_QUEUE,
+        )
+    await callback_query.answer()
