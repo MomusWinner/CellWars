176025b5535e1f459158acc3f5db49eb8c9e168f
spl3g <spleefer6@yandex.ru>
2024-12-23 23:47:42 +0300

    add movement callback

--- src/my_app/bot/types/callbacks.py
+++ src/my_app/bot/types/callbacks.py
@@ -13,13 +13,15 @@ class MenuCallback(BaseCallback, prefix="menu"):
     pass
 
 
-class PlacementCallback(CallbackData, prefix="place"):
-    cell_x: int
-    cell_y: int
-    type: str
-
-
 class FieldCallback(CallbackData, prefix="field"):
     cell_x: int
     cell_y: int
     type: str
+
+
+class PlacementCallback(FieldCallback, prefix="place"):
+    pass
+
+
+class MovementCallback(FieldCallback, prefix="move"):
+    pass
