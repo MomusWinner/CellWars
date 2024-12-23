e98020718daf1ed2a5cbd77a7b598bc2bf0c279f
spl3g <spleefer6@yandex.ru>
2024-12-24 01:20:58 +0300

    fix incorrect filtering

--- src/my_app/bot/handlers/callback/placement.py
+++ src/my_app/bot/handlers/callback/placement.py
@@ -4,8 +4,6 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, InlineKeyboardMarkup, Message
 from my_app.bot.composables.actions import add_cancel_button, add_field_actions
 from my_app.bot.composables.field import (
-    render_available_bank_placements,
-    render_available_warrior_placements,
     render_field,
 )
 from my_app.bot.handlers.buttons import (
@@ -16,6 +14,7 @@ from my_app.bot.handlers.buttons import (
 from my_app.bot.handlers.states.game import PlacementGroup
 from my_app.bot.types.callbacks import PlacementCallback
 from my_app.bot.types.game import GameTGMessage
+from my_app.bot.types.renderers import BankRenderer, WarriorsRenderer
 from my_app.bot.utils.field import rotate_coordinates
 from my_app.bot.utils.rabbit import publish_message
 from my_app.shared.game.game_logic.command import (
@@ -32,48 +31,56 @@ from my_app.shared.schema.messages.game import create_game_message
 from .router import router
 
 
-@router.callback_query(PLACE_WARRIORS_INLINE(), F.message.as_("message"), F.reply_markup.as_("reply_markup"))
+@router.callback_query(
+    PLACE_WARRIORS_INLINE(),
+    F.message.as_("message"),
+    F.message.reply_markup.as_("reply_markup"),
+    F.message.text.as_("text"),
+)
 async def warrior_placement_intent_handler(
+    callback_query: CallbackQuery,
     message: Message,
     reply_markup: InlineKeyboardMarkup,
+    text: str,
 ) -> None:
-    if message.text is None:
-        return
-
-    game_message = GameTGMessage.from_markup(message.text, reply_markup)
-    available_places = render_available_warrior_placements(game_message)
+    game_message = GameTGMessage.from_markup(text, reply_markup)
+    WarriorsRenderer(game_message).add_available_placements()
 
     game_message.actions = []
-    with_cancel = add_cancel_button(available_places)
+    add_cancel_button(game_message)
 
-    await message.edit_reply_markup(reply_markup=with_cancel.export_markup())
+    await message.edit_reply_markup(reply_markup=game_message.export_markup())
+    await callback_query.answer()
 
 
-@router.callback_query(PLACE_BANK_INLINE(), F.message.as_("message"), F.reply_markup.as_("reply_markup"))
+@router.callback_query(
+    PLACE_BANK_INLINE(),
+    F.message.as_("message"),
+    F.message.reply_markup.as_("reply_markup"),
+    F.message.text.as_("text"),
+)
 async def bank_placement_intent_handler(
+    callback_query: CallbackQuery,
     message: Message,
     reply_markup: InlineKeyboardMarkup,
+    text: str,
 ) -> None:
-    if message.text is None:
-        return
-
-    game_message = GameTGMessage.from_markup(message.text, reply_markup)
-    available_places = render_available_bank_placements(game_message)
+    game_message = GameTGMessage.from_markup(text, reply_markup)
+    BankRenderer(game_message).add_available_placements()
 
     game_message.actions = []
-    with_cancel = add_cancel_button(available_places)
+    add_cancel_button(game_message)
 
-    await message.edit_reply_markup(reply_markup=with_cancel.export_markup())
+    await message.edit_reply_markup(reply_markup=game_message.export_markup())
+    await callback_query.answer()
 
 
 @router.callback_query(CANCEL_FIELD_INLINE(), F.message.as_("message"))
 async def cancel_placement_handler(
+    callback_query: CallbackQuery,
     message: Message,
     state: FSMContext,
 ) -> None:
-    if message.text is None:
-        return
-
     data = await state.get_data()
     game_world_json: str = data["game_world"]
     game_world = json_to_game_world(game_world_json)
@@ -84,17 +91,21 @@ async def cancel_placement_handler(
     with_actions = add_field_actions(GameTGMessage.from_field(field_buttons))
 
     await message.edit_reply_markup(reply_markup=with_actions.export_markup())
+    await callback_query.answer()
 
 
 @router.callback_query(PlacementCallback.filter(F.type == "warrior"), F.message.as_("message"))
-async def place_warriors_handler(callback_data: PlacementCallback, message: Message, state: FSMContext) -> None:
+async def place_warriors_handler(
+    callback_query: CallbackQuery, callback_data: PlacementCallback, message: Message, state: FSMContext
+) -> None:
     await message.edit_text("Введите колличество:", reply_markup=None)
     await state.set_state(PlacementGroup.warriors)
     await state.update_data(warrior_place=(callback_data.cell_y, callback_data.cell_x))
+    await callback_query.answer()
 
 
 @router.callback_query(
-    PlacementCallback.filter(F.type == "bank"), F.message.as_("message"), F.reply_markup.as_("reply_markup")
+    PlacementCallback.filter(F.type == "bank"), F.message.as_("message"), F.message.reply_markup.as_("reply_markup")
 )
 async def place_bank_handler(
     callback_query: CallbackQuery,
@@ -129,7 +140,9 @@ async def place_bank_handler(
 
 
 @router.message(PlacementGroup.warriors, F.text.regexp(r"^(\d+)$").as_("count"), F.from_user.id.as_("user_id"))
-async def warrior_count_handler(message: Message, state: FSMContext, count: Match[str], user_id: int) -> None:
+async def warrior_count_handler(
+    callback_query: CallbackQuery, message: Message, state: FSMContext, count: Match[str], user_id: int
+) -> None:
     data = await state.get_data()
     room_id: str = data["room_id"]
     user_tag: int = data["user_tag"]
@@ -154,3 +167,4 @@ async def warrior_count_handler(message: Message, state: FSMContext, count: Matc
     )
 
     await message.delete()
+    await callback_query.answer()
